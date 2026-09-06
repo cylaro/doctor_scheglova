f3baf5f56872e32b0c424773a84f7d7d2f1f0e5d
cylaro <169532834+cylaro@users.noreply.github.com>
2026-09-06 22:15:52 +0300

    Update website

--- public/site-config.js
+++ public/site-config.js
@@ -12,12 +12,12 @@ window.SITE_CONFIG = {
   },
 
   contacts: {
-    phone: '+7 (915) 541-77-77',                      // Например: '+7 (999) 123-45-67'
+    phone: '',                      // Например: '+7 (999) 123-45-67'
     telegram: 'https://t.me/doctor_scheglova',                   // Например: 'https://t.me/your_username'
     telegramLabel: 'Telegram',      // Можно заменить на '@your_username'
     whatsapp: '',                   // Например: 'https://wa.me/79991234567'
     email: '',                      // Например: 'hello@example.ru'
-    bookingUrl: '',                 // Полная https-ссылка на запись; иначе используется телефон
+    bookingUrl: 'https://ogni.clinic/doctors/shcheglova_svetlana_vyacheslavovna',                 // Полная https-ссылка на запись; иначе используется телефон
     address: '',                    // Адрес клиники, если нужен
   },
 
